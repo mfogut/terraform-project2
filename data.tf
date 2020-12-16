8428e1581deb23744316f6dbcfa2495e47ba5151
mfogut <fatihogut27@gmail.com>
2020-12-16 12:07:49 -0500

    Ubuntu ami data added

--- data.tf
+++ data.tf
@@ -2,3 +2,23 @@
 data "aws_availability_zones" "AZs" {
   state = "available"
 }
+
+#Amazon Linux 2 AMI#########################
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["aws-marketplace"]
+
+  filter {
+    name   = "name"
+    values = ["Ubuntu Server 20.04 *"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
