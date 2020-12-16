58d5471da185302825ee195607d2f67a8dbcc2d7
mfogut <fatihogut27@gmail.com>
2020-12-16 13:09:00 -0500

    AppServers added

--- ec2_instances.tf
+++ ec2_instances.tf
@@ -1,3 +1,4 @@
+#Bashtion Host#################################
 resource "aws_instance" "bashtion_host" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.ec2_instance_type
@@ -9,7 +10,35 @@ resource "aws_instance" "bashtion_host" {
     Name = "Bashtion-Host"
   }
 }
+################################################
+#Private Subnet A App Servers
+resource "aws_instance" "app_server_a" {
+  ami                    = data.aws_ami.app_server.id
+  instance_type          = var.ec2_instance_type
+  subnet_id              = aws_subnet.private_subnet_a.id
+  vpc_security_group_ids = [aws_security_group.app_server_sg.id]
+  key_name               = "demo"
 
+  tags = {
+    Name = "AppServer-Host-${terraform.workspace}"
+  }
+}
+
+################################################
+#Private Subnet A App Servers
+resource "aws_instance" "app_server_b" {
+  ami                    = data.aws_ami.app_server.id
+  instance_type          = var.ec2_instance_type
+  subnet_id              = aws_subnet.private_subnet_b.id
+  vpc_security_group_ids = [aws_security_group.app_server_sg.id]
+  key_name               = "demo"
+
+  tags = {
+    Name = "AppServer-Host-${terraform.workspace}"
+  }
+}
+
+####################################################
 output "Bashtion_host_id" {
   value       = aws_instance.bashtion_host.id
   description = "Bastion host id"
@@ -18,4 +47,14 @@ output "Bashtion_host_id" {
 output "Bashtion_host_key_pair" {
   value       = aws_instance.bashtion_host.key_name
   description = "Bastion host id"
+}
+
+output "Appserver1_host_id" {
+  value       = aws_instance.app_server_a.id
+  description = "Private Subnet A AppServer host id"
+}
+
+output "Appserver2_host_id" {
+  value       = aws_instance.app_server_b.id
+  description = "Private Subnet B AppServer host id"
 }
