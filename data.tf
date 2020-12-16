5f84a55cc6dab93d214400fd711a59f376f7e2c7
mfogut <fatihogut27@gmail.com>
2020-12-16 13:08:01 -0500

    AppServer AMI data added

--- data.tf
+++ data.tf
@@ -22,3 +22,22 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 }
+
+#Red Hat Enterprise Linux 8#############
+data "aws_ami" "app_server" {
+  most_recent = true
+  owners      = ["aws-marketplace"]
+
+  filter {
+    name   = "name"
+    values = ["Red Hat Enterprise Linux 8 *"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
