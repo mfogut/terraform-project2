6485e513c33116e1447ec14158bb055f77d2f85c
mfogut <fatihogut27@gmail.com>
2020-12-16 15:43:02 -0500

    App Server added to locals

--- locals.tf
+++ locals.tf
@@ -5,3 +5,7 @@ locals {
 locals {
   app_server_subnets = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
 }
+
+locals {
+  app_servers = [aws_instance.app_server_a.id, aws_instance.app_server_b.id]
+}
